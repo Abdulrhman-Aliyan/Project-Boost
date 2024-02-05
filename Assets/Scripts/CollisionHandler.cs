225359dae38d86faeda62e3ac584ceda94d32c3a
Abdulrhman Aliyan <aboood1999@live.com>
2024-02-05 19:23:54 +0300

    next level load & stop control upon winning or crashing

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,24 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float delayTime = 1f;
     // Start is called before the first frame update
     void OnCollisionEnter(Collision other) 
     {
         var objCol = other.gameObject.tag;
         switch(other.gameObject.tag)
         {
-            case "Fuel":
-                Debug.Log(objCol);
-                break;
             case "Friendly":
                 Debug.Log(objCol);
                 break;
             case "Finish":
-                Debug.Log(objCol);
+                StartSuccessSequence();
                 break;
             default:
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                break; 
+                StartCrashSequence();
+                break;
         }
+    }
+
+    private void StartSuccessSequence(){
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel",delayTime);
+    }
+
+    private void StartCrashSequence(){
+        // todo add SFX sound upoun crash
+        // todo add particales effect upoun crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel",delayTime);
+    }
+
+    private void NextLevel()
+    {
+        int currenScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currenScene + 1;
+
+        if(SceneManager.sceneCountInBuildSettings == nextScene){
+            nextScene = 0;
+        }
+
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void ReloadLevel()
+    {
+        int currenScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currenScene);
     }
 }
